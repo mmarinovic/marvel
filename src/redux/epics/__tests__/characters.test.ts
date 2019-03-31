49acd5eaa611b497efeb526647ba32057104cad2
Marko <marinovic.marko@outlook.com>
2019-03-31 13:37:09 +0200

    added epic test

---
+++ src/redux/epics/__tests__/characters.test.ts
@@ -0,0 +1,47 @@
+import * as NS from '../../../namespace';
+
+import loadCharactersEpic from '../';
+import { TestScheduler } from 'rxjs/testing';
+import { IPagedResponse } from '../../../types/common';
+import { ICharacter } from '../../../types/models';
+
+describe('charcters epic', () => {
+
+    const testScheduler = new TestScheduler((actual, expected) => {
+        expect(actual).toEqual(expected);
+    });
+
+    it('should successfuly load characters', () => {
+        
+        const mockedMarvelApiResponse: IPagedResponse<ICharacter[]> = {
+            total: 0,
+            data: []
+        }
+
+        const loadCharactersActionType: NS.ILoadCharacters['type'] = 'LOAD_CHARACTERS';
+        const loadCharactersSuccessAction: NS.ILoadCharactersSuccess = {
+            type: 'LOAD_CHARACTERS_SUCCESS',
+            payload: mockedMarvelApiResponse
+        }
+
+        testScheduler.run(({ hot, cold, expectObservable }) => {
+            const action$:any = hot('-a', {
+              a: { type: loadCharactersActionType, payload: <any>{} }
+            });
+            const state$: any = null;
+            const dependencies: any = {
+              marvelApi: {
+                  loadCharacters: (_: any) => cold('--a', {
+                    a: mockedMarvelApiResponse
+                  })
+              }
+            };
+          
+            const output$ = loadCharactersEpic(action$, state$, dependencies);
+            
+            expectObservable(output$).toBe('--- 500ms a', {
+              a: loadCharactersSuccessAction
+            });
+          });
+    });
+})
